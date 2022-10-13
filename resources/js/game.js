e64a41069e41e8bd7f24eb394433be6b1b6c1f43
Jonathan Campbell <jpcampbell1127@gmail.com>
2022-10-13 13:47:06 -0700

    fixed color coding blocks

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,4 +1,3 @@
-
 const wordOfTheDayURL = 'https://words.dev-apis.com/word-of-the-day'
 let dailyWord = ''
 async function getWord() {
@@ -8,55 +7,99 @@ async function getWord() {
   dailyWord = processedWOD.word
 }
 
-
-
 function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
+  return /^[a-zA-Z]$/.test(letter)
 }
 
 const beginGame = getWord()
 
-
-
-
-let rowCount = 0
 let columnCount = 1
 let letterNum = 0
+let beginLetter = 0
 let testWord = ''
+let guessed = []
 
 const letterblock = document.getElementsByClassName('letterblock')
 const singleblock = document.getElementsByTagName('p')
 console.log(singleblock)
 
 document.addEventListener('keydown', function (event) {
-  if (event.code === 'Enter') {
-    checkAnswer()
-    /* Do something for Enter key */
-  } else if (event.code === 'Backspace' && letterNum > 0) {
-    singleblock[letterNum - 1].innerHTML = ''
-    testWord = testWord.slice(0, testWord.length - 1)
-    letterblock[letterNum - 1].style.borderColor = 'orange'
-    letterNum--
-  } else if (event.code === 'Backspace' && letterNum === 0) {
-    letterblock[letterNum - 1].style.borderColor = 'orange'
-    return
+  if (!isLetter(event.key)) {
+    if (
+      event.code === 'Backspace' &&
+      letterNum > 0 &&
+      letterNum > beginLetter
+    ) {
+      singleblock[letterNum - 1].innerHTML = ''
+      testWord = testWord.slice(0, testWord.length - 1)
+      letterblock[letterNum - 1].style.borderColor = 'orange'
+      letterNum--
+    } else if (event.code === 'Enter') {
+      checkAnswer()
+    }
   } else {
     if (testWord.length <= 4) {
-      singleblock[letterNum].innerHTML = event.key.toString()
-      testWord = testWord + event.key.toString()
+      
+      singleblock[letterNum].innerHTML = event.key.toString();
+      testWord = testWord + event.key.toString();
       letterblock[letterNum].style.borderColor = 'grey'
       letterNum = letterNum + 1
     }
   }
 })
 
-const checkAnswer = function () {
-  if (columnCount < 6) {
-    console.log(`You have pressed ENTER ${columnCount} times.`)
-    letterNum = columnCount + 4
-    testWord = ''
+const checkAnswer = () => {
+  if (testWord.length === 5) {
+    for (let i = 0; i <= testWord.length - 1; i++) {
+      for (let j = 0; j <= dailyWord.length - 1; j++) {
+        if (testWord[i] === dailyWord[j]) {
+          if (i === j) {
+            turnGreen(i)
+            guessed.push(testWord[i]);
+            
+          } else if (i != j) {
+            console.log("There are yellows here!")
+            turnYellow(i)
+            guessed.push(testWord[i]);
+            console.log(`pushed ${testWord[i]}`);
+          }
+        } 
+        
+      }
+    }
+    if (columnCount < 6) {
+      console.log(`You have pressed ENTER ${columnCount} times.`)
+      
+
+      beginLetter = letterNum
+      testWord = ''
+    } else {
+      columnCount++
+      console.log(`You have pressed ENTER ${columnCount} and LOST!`)
+    }
+  } 
+  guessed = [];
+}
+
+const turnGreen = function (index) {
+  currentSpot = beginLetter + index
+  letterblock[currentSpot].style.backgroundColor = 'green'
+}
+
+const turnYellow = function (index) {
+  if (guessed.includes(testWord[index]) === true) {
+    console.log("I am running the if part")
+    turnGrey(index)
   } else {
-    columnCount++
-    console.log(`You have pressed ENTER ${columnCount} and LOST!`)
+    console.log("I am running the else part")
+   
+   
+    guessed.push(testWord[index])
+    currentSpot = beginLetter + index
+    letterblock[currentSpot].style.backgroundColor = 'yellow'
   }
+}
+const turnGrey = function (index) {
+  currentSpot = beginLetter + index
+  letterblock[currentSpot].style.backgroundColor = 'grey'
 }
--- resources/js/word.js
+++ resources/js/word.js
@@ -7,57 +7,101 @@ async function getWord() {
   dailyWord = processedWOD.word
 }
 
-
-
 function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
+  return /^[a-zA-Z]$/.test(letter)
 }
 
 const beginGame = getWord()
 
-
-
-let startCount = 0;
-let rowCount = 0;
-let columnCount = 1;
-let letterNum = 0;
-let testWord = '';
+let columnCount = 1
+let letterNum = 0
+let beginLetter = 0
+let testWord = ''
+let guessed = []
 
 const letterblock = document.getElementsByClassName('letterblock')
 const singleblock = document.getElementsByTagName('p')
 console.log(singleblock)
 
-
-document.addEventListener('keydown',function(event) {
-
-            if (!isLetter(event.key)) {
-                if (event.code === 'Backspace' && letterNum > 0) {
-                    singleblock[letterNum - 1].innerHTML = ''
-                     testWord = testWord.slice(0, testWord.length - 1)
-                      letterblock[letterNum - 1].style.borderColor = 'orange'
-                      letterNum--
-                } else if (event.code === 'Enter') {
-                    checkAnswer();
-                }
-            } else {
-                if (testWord.length <= 4) {
-                    singleblock[letterNum].innerHTML = event.key.toString()
-                    testWord = testWord + event.key.toString()
-                    letterblock[letterNum].style.borderColor = 'grey'
-                    letterNum = letterNum + 1
-                  }
-            }
-        })
-
-
+document.addEventListener('keydown', function (event) {
+  if (!isLetter(event.key)) {
+    if (
+      event.code === 'Backspace' &&
+      letterNum > 0 &&
+      letterNum > beginLetter
+    ) {
+      singleblock[letterNum - 1].innerHTML = ''
+      testWord = testWord.slice(0, testWord.length - 1)
+      letterblock[letterNum - 1].style.borderColor = 'orange'
+      letterNum--
+    } else if (event.code === 'Enter') {
+      checkAnswer()
+    }
+  } else {
+    if (testWord.length <= 4) {
+      
+      singleblock[letterNum].innerHTML = event.key.toString();
+      testWord = testWord + event.key.toString();
+      letterblock[letterNum].style.borderColor = 'grey'
+      letterNum = letterNum + 1
+    }
+  }
+})
 
 const checkAnswer = () => {
-    if (columnCount < 6) {
-        console.log(`You have pressed ENTER ${columnCount} times.`)
-        letterNum = columnCount + 4
-        testWord = ''
-    } else {
-        columnCount++
-        console.log(`You have pressed ENTER ${columnCount} and LOST!`)
+  if (testWord.length === 5) {
+    
+    for (let i = 0; i <= testWord.length - 1; i++) {
+      turnGrey(i);
+      for (let j = 0; j <= dailyWord.length - 1; j++) {
+        if (testWord[i] === dailyWord[j]) {
+          if (i === j) {
+            turnGreen(i)
+            guessed.push(testWord[i]);
+            
+          } else if (i != j) {
+            console.log("There are yellows here!")
+            turnYellow(i)
+            guessed.push(testWord[i]);
+            console.log(`pushed ${testWord[i]}`);
+          }
+        } 
+        
+      }
     }
+    if (columnCount < 6) {
+      console.log(`You have pressed ENTER ${columnCount} times.`)
+      
+
+      beginLetter = letterNum
+      testWord = ''
+    } else {
+      columnCount++
+      console.log(`You have pressed ENTER ${columnCount} and LOST!`)
+    }
+  } 
+  guessed = [];
+}
+
+const turnGreen = function (index) {
+  currentSpot = beginLetter + index
+  letterblock[currentSpot].style.backgroundColor = 'green'
+}
+
+const turnYellow = function (index) {
+  if (guessed.includes(testWord[index]) === true) {
+    console.log("I am running the if part")
+    turnGrey(index)
+  } else {
+    console.log("I am running the else part")
+   
+   
+    guessed.push(testWord[index])
+    currentSpot = beginLetter + index
+    letterblock[currentSpot].style.backgroundColor = 'yellow'
+  }
+}
+const turnGrey = function (index) {
+  currentSpot = beginLetter + index
+  letterblock[currentSpot].style.backgroundColor = 'grey'
 }
